e155e2046d75bea0b14c00bc2fd2e82ff7256cab
VazquezDev37 <isaacvazquez1119@gmail.com>
2021-11-18 21:23:20 -0600

    update index and routes cart js file for shopping cart

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app = express();
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
-const productRoutes = require("./routes/product");
+//const productRoutes = require("./routes/product");
 const product = require('./routes/product');
+const cart = require('./routes/cart');
 
 
 connectDB();
@@ -13,8 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
+//app.use("/api/products", productRoutes);
 app.use('/api/product', product);
+app.use('/api/cart', cart);
 
 
 const port = process.env.PORT || 5000;
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,18 @@
+const Product = require('../models/Product');
+const { User }  = require('../models/User');
 const router = require("express").Router();
 
-
-
+router.post("/:userId/:prodId", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const product = await Product.findById(req.params.prodId);
+      user.shoppingCart.push(product);
+      await user.save();
+      return res.send(user);
+    } catch (ex) {
+      return res.status(500).send(`Internal Server Error:${ex}`);
+    }
+  });
 
 
 
