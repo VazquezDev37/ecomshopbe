ce721b0f1aa9745ed70ff9645f491b9ca2368e39
VazquezDev37 <isaacvazquez1119@gmail.com>
2021-11-21 21:56:37 -0600

    update stripe file

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,20 +1,32 @@
 const router = require("express").Router();
-const stripe = require("stripe");
+const Stripe = require("stripe")("Secret_key");
 
-router.post("/payment", async (req, res)=> {
-    stripe.charges.create({
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-        if (stripeErr) {
-            res.status(500).json(stripeErr);
-        } else {
-            res.status(201).json(stripeRes);
-        }
-    });
-});
+//router.post("/payment", async (req, res)=> {
+ //   stripe.charges.create({
+ //       source: req.body.tokenId,
+ //       amount: req.body.amount,
+ //       currency: "usd",
+ //   },
+  //  (stripeErr, stripeRes) => {
+  //      if (stripeErr) {
+  //          res.status(500).json(stripeErr);
+  //      } else {
+ //           res.status(201).json(stripeRes);
+ //       }
+ //   });
+//});
+
+router.post("/", async (req, res) => {
+    try {
+        const stripe =  Stripe(req.body);
+        
+      //stripe.shoppingCart.push(stripe);
+      await stripe.save();
+      return res.send(stripe);
+    } catch (ex) {
+      return res.status(500).send(`Internal Server Error:${ex}`);
+    }
+  });
 
 
 module.exports = router;
